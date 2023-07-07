62118540c7174c1b47c61a2735d9ff50ab144057
qing42102 <qing42102@outlook.com>
2023-07-06 20:52:39 -0500

    Add WGAN-GP.

--- conditional_GAN.py
+++ conditional_GAN.py
@@ -1,77 +1,177 @@
-import torch
-import torch.nn as nn
+import tensorflow as tf
+import keras.api._v2.keras as keras
 
 
-class Generator(nn.Module):
-    def __init__(self, latent_dim, label_dim):
-        super(Generator, self).__init__()
+class WGAN(keras.Model):
+    def __init__(
+        self,
+        discriminator,
+        generator,
+        latent_dim,
+        discriminator_extra_steps=3,
+        gp_weight=10.0,
+    ):
+        super().__init__()
+        self.discriminator = discriminator
+        self.generator = generator
+        self.latent_dim = latent_dim
+        self.d_steps = discriminator_extra_steps
+        self.gp_weight = gp_weight
 
-        self.label_embedding = nn.Sequential(
-            nn.Linear(label_dim, 32),
-            nn.ReLU(),
-            nn.Unflatten(dim=1, unflattened_size=(32, 1, 1)),
+    def compile(self, d_optimizer, g_optimizer, d_loss_fn, g_loss_fn):
+        super().compile()
+        self.d_optimizer = d_optimizer
+        self.g_optimizer = g_optimizer
+        self.d_loss_fn = d_loss_fn
+        self.g_loss_fn = g_loss_fn
+
+    def gradient_penalty(self, batch_size, real_images, fake_images):
+        """Calculates the gradient penalty.
+
+        This loss is calculated on an interpolated image
+        and added to the discriminator loss.
+        """
+        # Get the interpolated image
+        alpha = tf.random.normal([batch_size, 1, 1, 1], 0.0, 1.0)
+        diff = fake_images - real_images
+        interpolated = real_images + alpha * diff
+
+        with tf.GradientTape() as gp_tape:
+            gp_tape.watch(interpolated)
+            # 1. Get the discriminator output for this interpolated image.
+            pred = self.discriminator(interpolated, training=True)
+
+        # 2. Calculate the gradients w.r.t to this interpolated image.
+        grads = gp_tape.gradient(pred, [interpolated])[0]
+        # 3. Calculate the norm of the gradients.
+        norm = tf.sqrt(tf.reduce_sum(tf.square(grads), axis=[1, 2, 3]))
+        gp = tf.reduce_mean((norm - 1.0) ** 2)
+        return gp
+
+    def train_step(self, real_images):
+        if isinstance(real_images, tuple):
+            real_images = real_images[0]
+
+        batch_size = tf.shape(real_images)[0]
+
+        # For each batch, we are going to perform the
+        # following steps as laid out in the original paper:
+        # 1. Train the generator and get the generator loss
+        # 2. Train the discriminator and get the discriminator loss
+        # 3. Calculate the gradient penalty
+        # 4. Multiply this gradient penalty with a constant weight factor
+        # 5. Add the gradient penalty to the discriminator loss
+        # 6. Return the generator and discriminator losses as a loss dictionary
+
+        # Train the discriminator first. The original paper recommends training
+        # the discriminator for `x` more steps (typically 5) as compared to
+        # one step of the generator.
+        for i in range(self.d_steps):
+            random_latent_vectors = tf.random.normal(
+                shape=(batch_size, self.latent_dim)
+            )
+            with tf.GradientTape() as tape:
+                fake_images = self.generator(random_latent_vectors, training=True)
+
+                fake_logits = self.discriminator(fake_images, training=True)
+                real_logits = self.discriminator(real_images, training=True)
+
+                # Calculate the discriminator loss using the fake and real image logits
+                d_cost = self.d_loss_fn(real_img=real_logits, fake_img=fake_logits)
+
+                # Add the gradient penalty to the original discriminator loss
+                gp = self.gradient_penalty(batch_size, real_images, fake_images)
+                d_loss = d_cost + gp * self.gp_weight
+
+            # Get the gradients w.r.t the discriminator loss
+            d_gradient = tape.gradient(d_loss, self.discriminator.trainable_variables)
+            # Update the weights of the discriminator using the discriminator optimizer
+            self.d_optimizer.apply_gradients(
+                zip(d_gradient, self.discriminator.trainable_variables)
+            )
+
+        # Train the generator
+        random_latent_vectors = tf.random.normal(shape=(batch_size, self.latent_dim))
+        with tf.GradientTape() as tape:
+            generated_images = self.generator(random_latent_vectors, training=True)
+
+            gen_img_logits = self.discriminator(generated_images, training=True)
+            # Calculate the generator loss
+            g_loss = self.g_loss_fn(gen_img_logits)
+
+        # Get the gradients w.r.t the generator loss
+        gen_gradient = tape.gradient(g_loss, self.generator.trainable_variables)
+        # Update the weights of the generator using the generator optimizer
+        self.g_optimizer.apply_gradients(
+            zip(gen_gradient, self.generator.trainable_variables)
         )
+        return {"d_loss": d_loss, "g_loss": g_loss}
 
-        self.model = nn.Sequential(
-            nn.ConvTranspose2d(latent_dim + 32, 32 * 4, 4, 1, 0, bias=False),
-            nn.BatchNorm2d(32 * 4),
-            nn.ReLU(True),
-            nn.ConvTranspose2d(32 * 4, 32 * 2, 3, 2, 1, bias=False),
-            nn.BatchNorm2d(32 * 2),
-            nn.ReLU(True),
-            nn.ConvTranspose2d(32 * 2, 32, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(32),
-            nn.ReLU(True),
-            nn.ConvTranspose2d(32, 3, 4, 2, 1, bias=False),
-            nn.Sigmoid(),
+
+class Discriminator(keras.Model):
+    def __init__(self, shape, **kwargs):
+        super().__init__()
+        self.model = self.build_model()
+        self.shape = shape
+
+    def build_model(self):
+        model = keras.Sequential(
+            [
+                keras.Input(shape=self.shape),
+                keras.layers.Conv2D(64, kernel_size=4, strides=2, padding="same"),
+                keras.layers.LeakyReLU(alpha=0.2),
+                keras.layers.Conv2D(128, kernel_size=4, strides=2, padding="same"),
+                keras.layers.LeakyReLU(alpha=0.2),
+                keras.layers.Conv2D(256, kernel_size=4, strides=2, padding="same"),
+                keras.layers.LeakyReLU(alpha=0.2),
+                keras.layers.Conv2D(512, kernel_size=4, strides=2, padding="same"),
+                keras.layers.LeakyReLU(alpha=0.2),
+                keras.layers.Flatten(),
+                keras.layers.Dropout(0.2),
+                keras.layers.Dense(1),
+            ],
+            name="discriminator",
         )
+        return model
 
-    def forward(self, noise_vector, label):
-        label_output = self.label_embedding(label)
-
-        concat = torch.cat((noise_vector, label_output), dim=1)
-        image = self.model(concat)
-        return image
+    def call(self, inputs):
+        return self.model(inputs)
 
 
-class Discriminator(nn.Module):
-    def __init__(self, label_dim):
-        super(Discriminator, self).__init__()
+class Generator(keras.Model):
+    def __init__(self, latent_dim, **kwargs):
+        super().__init__()
+        self.latent_dim = latent_dim
+        self.model = self.build_model()
 
-        self.model = nn.Sequential(
-            nn.Conv2d(4, 32, 4, 2, 1, bias=False),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Conv2d(32, 32 * 2, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(32 * 2),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Conv2d(32 * 2, 32 * 4, 3, 2, 1, bias=False),
-            nn.BatchNorm2d(32 * 4),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Conv2d(32 * 4, 1, 4, 1, 0, bias=False),
-            nn.Sigmoid(),
+    def build_model(self):
+        model = keras.Sequential(
+            [
+                keras.Input(shape=(self.latent_dim,)),
+                keras.layers.Dense(2 * 2 * 256),
+                keras.layers.BatchNormalization(),
+                keras.layers.LeakyReLU(alpha=0.2),
+                keras.layers.Reshape((2, 2, 256)),
+                keras.layers.UpSampling2D(interpolation="bilinear"),
+                keras.layers.Conv2D(256, kernel_size=3, strides=1, padding="same"),
+                keras.layers.BatchNormalization(),
+                keras.layers.LeakyReLU(alpha=0.2),
+                keras.layers.UpSampling2D(interpolation="bilinear"),
+                keras.layers.Conv2D(128, kernel_size=3, strides=1, padding="same"),
+                keras.layers.BatchNormalization(),
+                keras.layers.LeakyReLU(alpha=0.2),
+                keras.layers.UpSampling2D(interpolation="bilinear"),
+                keras.layers.Conv2D(64, kernel_size=3, strides=1),
+                keras.layers.BatchNormalization(),
+                keras.layers.LeakyReLU(alpha=0.2),
+                keras.layers.UpSampling2D(interpolation="bilinear"),
+                keras.layers.Conv2D(
+                    3, kernel_size=3, strides=1, padding="same", activation="sigmoid"
+                ),
+            ],
+            name="generator",
         )
+        return model
 
-        self.label_embedding = nn.Sequential(
-            nn.Linear(label_dim, 784),
-            nn.ReLU(),
-            nn.Unflatten(dim=1, unflattened_size=(1, 28, 28)),
-        )
-
-    def forward(self, image, label):
-        label_output = self.label_embedding(label)
-        concat = torch.cat((image, label_output), dim=1)
-        output = self.model(concat)
-        output = torch.squeeze(output)
-        return output
-
-
-def generator_loss(label, fake_output):
-    binary_cross_entropy = nn.BCELoss()
-    gen_loss = binary_cross_entropy(label, fake_output)
-    return gen_loss
-
-
-def discriminator_loss(label, output):
-    binary_cross_entropy = nn.BCELoss()
-    disc_loss = binary_cross_entropy(label, output)
-    return disc_loss
+    def call(self, inputs):
+        return self.model(inputs)
